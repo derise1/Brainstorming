dae75794653a05bab9b4b7b9eb46b7c0cefcf9fa
Danila <bezrukov.2002@bk.ru>
2024-04-18 23:42:24 +0300

    Add Singleton for Scene Loader

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[DisallowMultipleComponent]
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    [SerializeField] protected bool isDontDestroyOnLoad = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (Reference == null)
+            {
+                if (!(Reference = FindObjectOfType<T>()))
+                {
+                    throw new MissingReferenceException($"The singleton reference to a {typeof(T).Name} does not found!");
+                }
+            }
+
+            return Reference;
+        }
+    }
+
+    public static bool HasReference
+    {
+        get
+        {
+            if (Reference == null)
+            {
+                return (Reference = FindObjectOfType<T>()) != null;
+            }
+
+            return true;
+        }
+    }
+
+    protected static T Reference;
+
+    protected virtual void Awake()
+    {
+        if (isDontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/UI/Scene_Loader.cs
+++ Assets/Scripts/UI/Scene_Loader.cs
@@ -1,10 +1,28 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Scene_Loader : MonoBehaviour
+public class Scene_Loader : Singleton<Scene_Loader>
 {
     public void LoadNextScene()
     {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadNextSceneAsync(SceneManager.GetActiveScene().buildIndex + 1).Forget();
+    }
+
+    private async UniTaskVoid LoadNextSceneAsync(int sceneIndex)
+    {
+        await UniTask.WaitUntil(() => TransitionEffect());
+        SceneManager.LoadSceneAsync(sceneIndex);
+    }
+
+    private bool TransitionEffect()
+    {
+        // DOTween.Sequence()
+        //     .Append()
+        //     .Play();
+
+        return true;
     }
 }
